d499f5195f35c705ef689b92b84982dab929cc7c
khalid02729 <khalidsalah4000@gmail.com>
2026-09-02 10:47:11 +0300

    Simplify server startup in server.py
    Refactored server startup code to streamline initialization.

--- server.py
+++ server.py
@@ -162,8 +162,5 @@ class ChatHandler(http.server.SimpleHTTPRequestHandler):
         return super().do_GET()
 
 if __name__ == '__main__':
-    import os
     port = int(os.environ.get('PORT', 8082))
-    server = http.server.ThreadingHTTPServer(('0.0.0.0', port), ChatHandler)
-    print(f"Server running on port {port}...")
-    server.serve_forever()
+    http.server.ThreadingHTTPServer(('0.0.0.0', port), ChatHandler).serve_forever()
